fe8fcfa334257fb57ee1f5828efb2aab0a69f88b
doyougnu <jmy6342@gmail.com>
2021-03-15 09:25:02 -0700

    forgot to add the files!

--- lib/rebuild.sh
+++ lib/rebuild.sh
@@ -8,7 +8,7 @@ command -v latexmk >/dev/null 2>&1 ||
     { echo >&2 "I couldn't find latexmk, which I use to compile. May I suggest: $ERR $nl Aborting."; exit 1; }
 
 # use --shell-escape for the minted package
-latexmk -pdf --shell-escape main.tex
+latexmk -pdf --shell-escape thesis.tex
 # latexmk -e '$pdflatex=q/pdflatex %O -shell-escape %S/' -pdf
 # bibtex vsat_paper.tex
 # latexmk -pdf vsat_paper.tex
